7896cb87304ebb0c6da764345923f661435d8fe4
Ronnie Rios <ronniesmail14@gmail.com>
2022-11-23 14:30:04 -0600

    initial model and mongo connection create

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
+//imports
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+//express 
 const app = express();
 const port = 9001;
 app.use(express.json());
 
+//mongoose connection
+mongoose.connect('mongodb://localhost/todo-prac');
+
+//express controllers
+
+//Express Listener
 const listener = () => {
     console.log(`server on ${port}`);
 }
